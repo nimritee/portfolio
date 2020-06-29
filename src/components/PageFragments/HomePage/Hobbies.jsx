089f0be10b563e921c9558ef5587407b17311006
nimritee <nimritee97@gmail.com>
2020-06-30 04:22:12 +0530

    handle small hobby class change

--- src/components/PageFragments/HomePage/Hobbies.jsx
+++ src/components/PageFragments/HomePage/Hobbies.jsx
@@ -3,35 +3,36 @@ import { Row, Col } from 'antd';
 import AboutTile from '../../AbouTile';
 
 const Hobbies = () => (
-    <div>
-        <br/>
-        <h2 className="titleSeparate">My Interests</h2>
-        <Row gutter={[20, 20]}>
-        <Col xs={24} sm={24} md={12} lg={8}>
-          <AboutTile
-            img="coffee.png"
-            alt="coffee image"
-            textH4="Love Coffee"
-            textH3="Coffee + Me = Happiness"
-          />
-        </Col>
-        <Col xs={24} sm={24} md={12} lg={8}>
-          <AboutTile
-            img="swimming.png"
-            alt="swimming image"
-            textH4="Love Swimming"
-            textH3="At times"
-          />
-        </Col>
-        <Col xs={24} sm={24} md={12} lg={8}>
-          <AboutTile
-            img="car.png"
-            alt="car image"
-            textH4="Love Driving"
-            textH3="Therapy of life"
-          />
-        </Col>
-      </Row>
-    </div>
+  <div>
+    <br />
+    <h2 className="titleSeparate">My Interests</h2>
+    <Row gutter={[20, 20]}>
+      <Col xs={24} sm={24} md={12} lg={8}>
+        <AboutTile
+          img="coffee.png"
+          alt="coffee image"
+          textH4="Love Coffee"
+          textH3="Coffee + Me = Happiness"
+        />
+      </Col>
+      <Col xs={24} sm={24} md={12} lg={8}>
+        <AboutTile
+          className="hobbies_class"
+          img="swimming.png"
+          alt="swimming image"
+          textH4="Love Swimming"
+          textH3="At times"
+        />
+      </Col>
+      <Col xs={24} sm={24} md={12} lg={8}>
+        <AboutTile
+          img="car.png"
+          alt="car image"
+          textH4="Love Driving"
+          textH3="Therapy of life"
+        />
+      </Col>
+    </Row>
+  </div>
 );
 export default Hobbies;

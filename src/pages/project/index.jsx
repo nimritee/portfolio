268ab0563be9e94f54dc9a7e5ef9c9c6e82b9c4a
nimritee <nimritee97@gmail.com>
2020-06-30 06:14:13 +0530

    add prop

--- src/pages/project/index.jsx
+++ src/pages/project/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import { Layout, Row, Col } from 'antd';
 import Header from '../../components/PageLayout/Header';
@@ -36,6 +37,14 @@ const Project = ({ data }) => (
     </Layout>
   </Layout>
 );
+
+Project.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export const query = graphql`
   {

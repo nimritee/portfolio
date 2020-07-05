a3b21f55a7554e3f8f9bb64ed2e1e7baf9eda92a
nimritee <nimritee97@gmail.com>
2020-07-06 01:00:11 +0530

    Modify the blogs page and add tags

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import moment from 'moment';
 import { Link } from 'gatsby';
 import style from './postCard.module.less';
+import { Tag, Divider } from 'antd';
 
 const PostCard = (props) => {
   const { data: { node: { frontmatter } } } = props;
@@ -9,22 +10,28 @@ const PostCard = (props) => {
   return (
     <div className={style.postCard}>
       <Link to={`/${frontmatter.path}`}>
-        <div
-          className={style.postCardImg}
-          style={{
-            backgroundImage: `url(${frontmatter ? frontmatter.cover.childImageSharp.fluid.src : ''})`,
-          }}
-        />
         <div className={style.mrTp20}>
+        <h3>{frontmatter ? frontmatter.title : ''}</h3>
           <p>
             <span className={style.dateHolder}>{frontmatter ? moment(frontmatter.date).format('MMM Do YYYY') : ''}</span>
           </p>
-          <h3>{frontmatter ? frontmatter.title : ''}</h3>
           <p>{frontmatter ? frontmatter.excerpt : ''}</p>
           <p style={{ color: '#ce6d96', wordSpacing: '10px' }}>
-            {
-                `#${frontmatter.tags.join(' #')}`
-            }
+          #${frontmatter.tags.join(' #')}
+
+          <div>
+            <Tag color="magenta">java</Tag>
+            <Tag color="red">css</Tag>
+            <Tag color="volcano">php</Tag>
+            <Tag color="orange">selenium</Tag>
+            <Tag color="gold">gold</Tag>
+            <Tag color="lime">lime</Tag>
+            <Tag color="green">green</Tag>
+            <Tag color="cyan">cyan</Tag>
+            <Tag color="blue">blue</Tag>
+            <Tag color="geekblue">geekblue</Tag>
+            <Tag color="purple">purple</Tag>
+          </div>
           </p>
         </div>
       </Link>

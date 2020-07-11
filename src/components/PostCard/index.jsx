a26d8728e2406ef7fbdeb8b17f0534647294494e
nimritee <nimritee97@gmail.com>
2020-07-11 23:43:12 +0530

    Add images

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,37 +1,49 @@
 import React from 'react';
 import moment from 'moment';
 import { Link } from 'gatsby';
+import { Tag } from 'antd';
 import style from './postCard.module.less';
-import { Tag, Divider } from 'antd';
 
 const PostCard = (props) => {
   const { data: { node: { frontmatter } } } = props;
+  const color = ["red","blue","green"];
 
   return (
     <div className={style.postCard}>
       <Link to={`/${frontmatter.path}`}>
         <div className={style.mrTp20}>
-        <h3>{frontmatter ? frontmatter.title : ''}</h3>
+        <div
+          className={style.postCardImg}
+          style={{
+            backgroundImage: `url(${frontmatter ? frontmatter.cover.childImageSharp.fluid.src : ''})`,
+          }}
+        />
+          <h3>{frontmatter ? frontmatter.title : ''}</h3>
           <p>
             <span className={style.dateHolder}>{frontmatter ? moment(frontmatter.date).format('MMM Do YYYY') : ''}</span>
           </p>
           <p>{frontmatter ? frontmatter.excerpt : ''}</p>
-          <p style={{ color: '#ce6d96', wordSpacing: '10px' }}>
-          #${frontmatter.tags.join(' #')}
+          {
+            color.map((color,i) =>  {
+              return <Tag color ={color} key  = {'color' - i }/>
+            })
+          }
+          ${frontmatter.tags.join(' #')}
 
-          <div>
-            <Tag color="magenta">java</Tag>
-            <Tag color="red">css</Tag>
-            <Tag color="volcano">php</Tag>
-            <Tag color="orange">selenium</Tag>
-            <Tag color="gold">gold</Tag>
-            <Tag color="lime">lime</Tag>
-            <Tag color="green">green</Tag>
-            <Tag color="cyan">cyan</Tag>
-            <Tag color="blue">blue</Tag>
-            <Tag color="geekblue">geekblue</Tag>
-            <Tag color="purple">purple</Tag>
-          </div>
+          <p style={{ color: '#ce6d96', wordSpacing: '10px' }}>
+            <div>
+              <Tag color="magenta">java</Tag>
+              <Tag color="red">css</Tag>
+              <Tag color="volcano">php</Tag>
+              <Tag color="orange">selenium</Tag>
+              <Tag color="gold">gold</Tag>
+              <Tag color="lime">lime</Tag>
+              <Tag color="green">green</Tag>
+              <Tag color="cyan">cyan</Tag>
+              <Tag color="blue">blue</Tag>
+              <Tag color="geekblue">geekblue</Tag>
+              <Tag color="purple">purple</Tag>
+            </div>
           </p>
         </div>
       </Link>

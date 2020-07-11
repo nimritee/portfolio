75dcb69878951543c824668718b545d20a8e68a9
nimritee <nimritee97@gmail.com>
2020-07-12 01:29:02 +0530

    Set blogs config

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -3,26 +3,33 @@ import moment from 'moment';
 import { Link } from 'gatsby';
 import { Tag } from 'antd';
 import style from './postCard.module.less';
+import { Row, Col } from 'antd';
 
-const ImgStyle =  {
-  boxShadow: "1px 3px 16px #a7a7a7",
-  borderRadius: "10px",
-  height: "164px"
-}
+
+const ImgStyle = {
+  boxShadow: '1px 3px 16px #a7a7a7',
+  borderRadius: '10px',
+  maxWidth: '90%',
+};
 
 const PostCard = (props) => {
   const { data: { node: { frontmatter } } } = props;
-  const colorArray = {"testing":"magenta","automated testing":"red","selenium":"volcano","html":"orange","css":"gold"};
+  const colorArray = { "testing": "magenta", "automated testing": "red", "selenium": "volcano", "html": "orange", "css": "gold" };
 
   return (
     <div className={style.postCard}>
       <Link to={`/${frontmatter.path}`}>
-        <div>
+        <Row align="middle">
+          <Col Xs={24} sm={24} md={8} lg={8}>
 
-          {frontmatter ? <img style={ImgStyle} src={frontmatter.cover.childImageSharp.fluid.src}/> : null}
-          
-          <div className={style.mrTp20}>
-            <h3>{frontmatter ? frontmatter.title : ''}</h3>
+            {frontmatter ? <img style={ImgStyle} src={frontmatter.cover.childImageSharp.fluid.src} /> : null}
+
+          </Col>
+          <Col xs={24} sm={24} md={16} lg={16}>
+            <div className={style.mrTp20}>
+              <h3>{frontmatter ? frontmatter.title : ''}</h3>
+            </div>
+
             <p>
               <span className={style.dateHolder}>{frontmatter ? moment(frontmatter.date).format('MMM Do YYYY') : ''}</span>
             </p>
@@ -31,14 +38,14 @@ const PostCard = (props) => {
             <p style={{ color: '#ce6d96', wordSpacing: '10px' }}>
               <div>
                 {
-                  frontmatter.tags.map(function(innerData,index){
+                  frontmatter.tags.map(function (innerData) {
                     return (<Tag color={colorArray[innerData]} index={innerData}>{innerData}</Tag>)
                   })
                 }
               </div>
             </p>
-          </div>
-        </div>
+          </Col>
+        </Row>
       </Link>
     </div>
   );
